29288d6b257cfffff67bef723ff59606cb565907
SnehalChodankar <snehalchodankar95@gmail.com>
2024-04-28 11:14:56 +0530

    auth changes

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
+import os
+from twilio.rest import Client
 
-API_KEY = "2b85e1a21adf9ca443a2b2f59730f7d6"
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+
+
+API_KEY = os.environ.get("OWM_API_KEY")
 
 parameters = {
-    "lat": 29.392060,
-    "lon": -98.543700,
+    "lat": 15.645460,
+    "lon": 73.828033,
     "cnt": 4,
     "appid": API_KEY
 }
@@ -12,10 +18,12 @@ parameters = {
 response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
 response.raise_for_status()
 data = response.json()
-list = data["list"]
+list_data = data["list"]
 print(data)
 
-for item in list:
+
+will_rain = False
+for item in list_data:
     weather = item["weather"]
     weather_dict = weather[0]
 
@@ -23,4 +31,19 @@ for item in list:
     # print(weather_id)
 
     if int(weather_id) < 7:
-        print("Bring an Umbrella!!!")
+        will_rain = True
+
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        body="Its going to rain, bring an umbrella",
+        from_='+17193575948',
+        to='+919284832649'
+    )
+
+    print(message.status)
+else:
+
+    print("Its not raining today!!!")
